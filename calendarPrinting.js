d67210a23f9840f34aacee1ec8f40ad05535a7eb
vincent.etien <vincent.etien@insa-rennes.fr>
2019-12-29 13:02:19 +0100

    day 6 + reorganised files

---
+++ calendarPrinting.js
@@ -0,0 +1,42 @@
+//adds one day to the date
+Date.prototype.addOneDay = function() {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + 1);
+    return date;
+}
+//returns the date to print with format yyyymmdd
+Date.prototype.yyyymmdd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+  
+    return [(dd>9 ? '' : '0') + dd+"/",
+            (mm>9 ? '' : '0') + mm+"/",
+            this.getFullYear()
+           ].join('');
+  };
+
+//Prints my wonderfull calendar
+function printCalendar(calendar){
+    let startingDate = new Date(2019,11,24);
+    let output = "";
+    let i=0;
+    let today = new Date();
+    while (startingDate<today) {
+        output += 
+        "\n***************\n* "+startingDate.yyyymmdd()+"  * ==>  "+calendar[i]+"\n***************\n";
+        startingDate = startingDate.addOneDay();
+        i++;
+    }
+    console.log(output);
+}
+
+let myCalendar = [
+    "Started printing calendar",
+    "Finished printing calendar",
+    "Piet operations reader",
+    "Piet Op reader cycle & hextoHueLum Converter",
+    "HextoHueLum Converter & understood piet CC & DP",
+    "Started using git, began the main piet interpreter"
+]
+
+printCalendar(myCalendar);
--- pietInterpreter.js
+++ pietInterpreter.js
@@ -1,44 +1,3 @@
-//adds one day to the date
-Date.prototype.addOneDay = function() {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + 1);
-    return date;
-}
-//returns the date to print with format yyyymmdd
-Date.prototype.yyyymmdd = function() {
-    var mm = this.getMonth() + 1; // getMonth() is zero-based
-    var dd = this.getDate();
-  
-    return [(dd>9 ? '' : '0') + dd+"/",
-            (mm>9 ? '' : '0') + mm+"/",
-            this.getFullYear()
-           ].join('');
-  };
-
-//Prints my wonderfull calendar
-function printCalendar(calendar){
-    let startingDate = new Date(2019,11,24);
-    let output = "";
-    let i=0;
-    let today = new Date();
-    while (startingDate<today) {
-        output += 
-        "\n***************\n* "+startingDate.yyyymmdd()+"  * ==>  "+calendar[i]+"\n***************\n";
-        startingDate = startingDate.addOneDay();
-        i++;
-    }
-    console.log(output);
-}
-
-let myCalendar = [
-    "Started printing calendar",
-    "Finished printing calendar",
-    "Piet operations reader",
-    "Piet Op reader cycle & hextoHueLum Converter",
-    "HextoHueLum Converter & understood piet CC & DP"
-]
-
-printCalendar(myCalendar);
 
 
 function hexToHueLum(hexcode){
@@ -59,17 +18,25 @@ function hexToHueLum(hexcode){
     "00C000":["dark","green"],
     "00C0C0":["dark","cyan"],
     "0000C0":["dark","blue"],
-    "C000C0":["dark","magenta"]}
+    "C000C0":["dark","magenta"],
+    "FFFFFF":["white","white"],
+    "000000":["black","black"]}
     return conversionTable[hexcode];
 }
 
+function testfuncObj(){
+    console.log("swag");
+}
 function getOperation (formerlight,formerhue,light,hue){
     let hues=["red","yellow","green","cian","blue","magenta"];
     let lums=["light","normal","dark"];
-    let ops=[["","add","divide","greater","duplicate","inchar"],
+    let ops=[["nothing","add","divide","greater","duplicate","inchar"],
             ["push","substract","mod","pointer","roll","outnum"],
             ["pop","multiply","not","switch","innum","outchar"]]
     let hueval,lumval;
+    if(formerlight=="white"||light=="white"){
+        return testfuncObj;
+    }
     if(lums.indexOf(light)-lums.indexOf(formerlight)<0){
         lumval = 3- lums.indexOf(light)-lums.indexOf(formerlight);
     }else{
@@ -83,3 +50,22 @@ function getOperation (formerlight,formerhue,light,hue){
     return ops[lumval][hueval]
 }
 
+function readPiet(imageArray){
+    let cc=1; // values of cc -> -1 for left 1 for right
+    let dp=0; // values of dp 0-> right 1-> bottom , 2-> left 3->top
+    let x=0;
+    let y=0;
+    let pastColor;
+    let currentColor;
+    let keepRunning=true;
+    while(keepRunning){
+        pastColor=imageArray[x][y];
+        /* update x and y */
+        currentColor=imageArray[x][y];
+        let [pl,ph] = hexToHueLum(pastColor);
+        let [cl,ch] = hexToHueLum(currentColor)
+        getOperation(pl,ph,cl,ch)();
+    }
+}
+let imageArray=[["FFFFFF"]];
+readPiet(imageArray);
